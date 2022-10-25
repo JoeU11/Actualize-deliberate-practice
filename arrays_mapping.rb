a1124c9f9ac14e1539caa645ea641ce15ea4014f
joe <joe.ulica@gmail.com>
2022-10-25 12:43:28 -0400

    redo arrays_select in ruby

--- arrays_mapping.rb
+++ arrays_mapping.rb
@@ -16,85 +16,113 @@
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
 # array = ["hello", "goodbye", "yesterday"]
-# array_2 = array.map do |c|
-#   c.upcase
-# end
-# p array_2
-
-
+# # array_2 = array.map do |c|
+# #   c.upcase
+# # end
+# # p array_2
+# array2 = []
+# array.each { |word| array2.push(word.upcase)}
+# p array2
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
 # array = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
-# array_2 = array.map do |c|
-#   c[:name]
-# end
-# p array_2
+# # array_2 = array.map do |c|
+# #   c[:name]
+# # end
+# # p array_2
+
+# array3 = array.map { |person| person[:name]}
+# p array3
+
 
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
 
 # array = [1, 2, 3, 4,]
-# array_2 = array.map do |c|
-#   c + 7
-# end
-# p array_2
+# # array_2 = array.map do |c|
+# #   c + 7
+# # end
+# # p array_2
+
+# array3 = array.map { |num| num + 7}
+# p array3
+
+
 
 #  5. Start with an array of strings and create a new array with each string's length.
-#     For example, ["hello", "goodbye"] becomes [5, 7].
+# #     For example, ["hello", "goodbye"] becomes [5, 7].
 
 # array = ["hello", "goodbye"]
-# array_2 = []
-# array.each do |c|
-#   array_2 << c.length
-# end
-# p array_2
+# # array_2 = []
+# # array.each do |c|
+# #   array_2 << c.length
+# # end
+# # p array_2
+
+# array3 = array.map { |word| word.length}
+# p array3
 
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
-#     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+# #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
 # array = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
-# array_2 = array.map do |c|
-#   c[:age]
-# end
-# p array_2
+# # array_2 = array.map do |c|
+# #   c[:age]
+# # end
+# # p array_2
+
+# array3 = array.map { |person| person[:age]}
+# p array3
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
 # array = [1, 2, 3, 4, 5]
-# array_2 = array.map do |c|
-#   c / 2.0
-# end
-# p array_2
+# # array_2 = array.map do |c|
+# #   c / 2.0
+# # end
+# # p array_2
+
+# array3 = array.map { |num| num / 2.0}
+# p array3
 
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #     For example, ["hello", "goodbye"] becomes ["h", "g"].
 
 # array = ["hello", "goodbye"]
-# array_2 = array.map do |c|
-#   c.slice(0)
-# end
-# p array_2
+# # array_2 = array.map do |c|
+# #   c.slice(0)
+# # end
+# # p array_2
+
+# array3 = array.map { |word| word[0]}
+# p array3
 
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
 # array = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
-# array_2 = array.map do |c|
-#   c[:age] * 2
-# end
-# p array_2
+# # array_2 = array.map do |c|
+# #   c[:age] * 2
+# # end
+# # p array_2
+
+# array3 = array.map { |person| person[:age] * 2}
+# p array3
 
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
 
 # array = [1, 2, 3, 4, 5]
-# array_2 = array.map do |c|
-#   c.to_s
-# end
-# p array_2
+# # array_2 = array.map do |c|
+# #   c.to_s
+# # end
+# # p array_2
+
+# array3 = array.map { |num| num.to_s}
+# p array3
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/b9ac4390aad2301a2238efc95c904f3d
 # SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/66598fd7cdbc67fe663624e217cebbaf
--- arrays_select.rb
+++ arrays_select.rb
@@ -1,13 +1,16 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 # array = [2, 32, 80, 18, 12, 3]
-# # array_2 = []
-# # array.each do |c|
-# #   if c < 20
-# #     array_2 << c
-# #   end
-# # end
-# # p array_2
+# # # array_2 = []
+# # # array.each do |c|
+# # #   if c < 20
+# # #     array_2 << c
+# # #   end
+# # # end
+# # # p array_2
+
+# array3 = array.select { |num| num < 20}
+# p array3
 
 # array_2 = array.select do |c|
 #   c < 20
@@ -18,79 +21,96 @@
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
 # array = ["winner", "winner", "chicken", "dinner"]
-# array_2 = array.select do |c|
-#   c[0] == "w"
-# end
-# p array_2
+# # array_2 = array.select do |c|
+# #   c[0] == "w"
+# # end
+# # p array_2
+
+# p array.select { |word| word[0].downcase == "w"}
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
 # array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
-# array_2 = array.select do |c|
-#   c[:price] > 5
-# end
-# p array_2
+# # array_2 = array.select do |c|
+# #   c[:price] > 5
+# # end
+# # p array_2
 
+# p array.select { |item| item[:price] > 5}
 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
 # array = [2, 4, 5, 1, 8, 9, 7]
-# array_2 = array.select do |c|
-#   c.even?
-# end
-# p array_2
+# # array_2 = array.select do |c|
+# #   c.even?
+# # end
+# # p array_2
+
+# p array.select { |num| num % 2 == 0}
 
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
 # array = ["a", "man", "a", "plan", "a", "canal", "panama"]
-# array_2 = array.select do |c|
-#   c.length < 4
-# end
-# p array_2
+# # array_2 = array.select do |c|
+# #   c.length < 4
+# # end
+# # p array_2
+
+# p array.select { |string| string.length < 4}
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
 # array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
-# array_2 = array.select do |c|
-#   c[:name].length < 6
-# end
+# # array_2 = array.select do |c|
+# #   c[:name].length < 6
+# # end
+
+# p array.select { |item| item[:name].length < 6}
 
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
 # array = [8, 23, 0, 44, 1980, 3]
-# array_2 = array.select do |c|
-#   c < 10
-# end
+# # array_2 = array.select do |c|
+# #   c < 10
+# # end
+
+# p array.select { |num| num < 10}
 
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
 # array = ["big", "little", "good", "bad"]
-# array_2 = array.select do |c|
-#   c[0] != "b"
-# end
+# # array_2 = array.select do |c|
+# #   c[0] != "b"
+# # end
+
+# p array.select { |word| word[0] != "b"}
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
 # array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
-# array_2 = array.select do |c|
-#   c[:price] < 10
-# end
+# # array_2 = array.select do |c|
+# #   c[:price] < 10
+# # end
+
+# p array.select { |item| item[:price] < 10}
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
 
-# array = [2, 4, 5, 1, 8, 9, 7]
+array = [2, 4, 5, 1, 8, 9, 7]
 # array_2 = array.select do |c|
 #   c.odd?
 # end
 # p array_2
+
+p array.select { |num| num % 2 == 1}
 
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/7de16ed43ea506e98df3fa15074b84f8
